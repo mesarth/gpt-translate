4060ba5ee71656c65ef08153bc462b643ad002f4
Tobias Schranz <tobias.g.schranz@gmail.com>
2024-01-16 09:40:50 +0100

    stub out translation card

--- app/(tabs)/main.tsx
+++ app/(tabs)/main.tsx
@@ -1,12 +1,26 @@
-import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '~/components/ui/card';
+import { Input } from '~/components/ui/input';
+import { Separator } from '~/components/ui/separator';
+import { Textarea } from '~/components/ui/textarea';
 
 export default function MainScreen() {
   const insets = useSafeAreaInsets();
   return (
     <View className='flex-1 justify-center items-center'>
-      <Text className='text-foreground text-xl'>Comming soon...</Text>
+      <Card>
+        <CardHeader>
+          <CardTitle>Translate</CardTitle>
+          <CardDescription>Translate text from one language to another.</CardDescription>
+        </CardHeader>
+        <CardContent className='flex gap-3'>
+          <Textarea placeholder='Enter Text'/>
+          <Separator className='w-full'/>
+          <Textarea className='opacity-50' placeholder='The translation will appear here' editable={false}/>
+        </CardContent>
+      </Card>
     </View>
   );
 }
