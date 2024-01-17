df51adfaabc12de58f3438fe4b3bcf0c548ce489
Tobias Schranz <tobias.g.schranz@gmail.com>
2024-01-18 00:53:47 +0100

    favorites screen stub out

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -1,17 +1,57 @@
-import { Text } from 'react-native';
+import { FlatList, Text } from 'react-native';
 import Container from '../components/container';
 import { StorageService, Translation, useTranslationStore } from '~/service/storage.service';
 import { useState, useEffect } from 'react';
+import { Card, CardContent, CardTitle } from '~/components/ui/card';
+import { Separator } from '~/components/ui/separator';
 
 export default function FavoritesScreen() {
   const translations = useTranslationStore(state => state.translations);
+  const favorites:any = [];
 
   return (
     <>
       <Container title='Favorites'>
+        <FlatList
+          className='w-full'
+          data={favorites}
+          ListEmptyComponent={() => <Text className='text-foreground text-xl text-center mt-10'>No favorites yet...</Text>}
+          renderItem={({item}) => 
+          <Card className='p-4'>
+            <CardTitle>
+              <Text className='text-foreground text-2xl'>
+                {item.inputLanguage} - {item.outputLanguage}
+              </Text>
+            </CardTitle>
+            <CardContent className='py-4 flex gap-2'>
+              <Text>{item.input}</Text>
+              <Separator className='w-full'/>
+              <Text>{item.output}</Text>
+            </CardContent>
+          </Card>
+        }
+        />
       </Container>
       <Container title='Recents'>
-        <Text>{translations.length}</Text>
+        <FlatList
+          className='w-full'
+          data={translations}
+          ListEmptyComponent={() => <Text className='text-foreground text-xl'>No translations yet...</Text>}
+          renderItem={({item}) => 
+          <Card className='p-4'>
+            <CardTitle>
+              <Text className='text-foreground text-2xl'>
+                {item.inputLanguage} - {item.outputLanguage}
+              </Text>
+            </CardTitle>
+            <CardContent className='py-4 flex gap-2'>
+              <Text>{item.input}</Text>
+              <Separator className='w-full'/>
+              <Text>{item.output}</Text>
+            </CardContent>
+          </Card>
+        }
+        />
       </Container>
     </>
   );
