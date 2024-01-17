e01a5a016d1f108d2756f9cf0e415ecfbeab2261
Tobias Schranz <tobias.g.schranz@gmail.com>
2024-01-17 21:51:41 +0100

    add disabled button info

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useState } from 'react';
 import { View, Text } from 'react-native';
+import Toast from 'react-native-toast-message';
 import { Button } from '~/components/ui/button';
 import { Card, CardContent } from '~/components/ui/card';
 import { Combobox, ComboboxOption } from '~/components/ui/combobox';
 import { Separator } from '~/components/ui/separator';
-import { Switch } from '~/components/ui/switch';
 import { Textarea } from '~/components/ui/textarea';
 import { TranslationResponse, languages, translate } from '~/service/translation.service';
 
@@ -29,6 +29,18 @@ export default function MainScreen() {
     return input.length > 3 && selectedLanguage?.value;
   }
 
+  const showDisabledButtonToast = () => {
+    Toast.show({
+      type: 'info',
+      text1: 'Please enter text and select a language',
+      visibilityTime: 3000,
+      autoHide: true,
+      swipeable: true,
+      bottomOffset: 100,
+      position: 'bottom',
+    });
+  };
+
   return (
     <View className='flex-1 justify-center items-center p-10'>
       <Text className='text-2xl native:text-4xl font-semibold text-foreground text-left w-full mb-2'>Translate</Text>
@@ -37,7 +49,7 @@ export default function MainScreen() {
           <Textarea placeholder='Enter Text' onChangeText={(t) => setInput(t)}/>
           <Separator className='w-full'/>
           <Combobox selectedItem={selectedLanguage} items={languagesOptions} onSelectedItemChange={setSelectedLanguage} placeholder='Select Language'/>
-          <Button className='w-full mt-3' onPress={handleTranslate} disabled={!translationActivated()}>Translate</Button>
+          <Button className='w-full mt-3' onPress={handleTranslate} disabled={!translationActivated()} onTouchStart={() => showDisabledButtonToast()}>Translate</Button>
           <Text className='text-lg'>{output}</Text>
         </CardContent>
       </Card>
