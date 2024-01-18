8bc64994323ed69ac9ad5e18d595a103f8709f22
Tobias Schranz <tobias.g.schranz@gmail.com>
2024-01-18 23:05:09 +0100

    add toggle icon

---
+++ app/components/ToggleIcon.tsx
@@ -0,0 +1,44 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Pressable } from 'react-native';
+import Animated from 'react-native-reanimated';
+import Transition,{ useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+
+interface Props {
+  toggled?: boolean;
+  onPress?: () => void;
+  First: React.ReactNode;
+  Second: React.ReactNode;
+  time?: number;
+}
+
+export default function ToggleIcon({ toggled = false, onPress, First, Second, time }: Props) {
+  const [firstActive, setFirstActive] = useState(true);
+
+  console.log("toggled ", toggled);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (!firstActive) {
+      timer = setTimeout(() => {
+        setFirstActive(true);
+      }, time);
+    }
+    return () => clearTimeout(timer);
+  }, [firstActive, time]);
+
+  const action = () => {
+    if(toggled){
+      onPress && onPress();
+    } 
+    else if(firstActive || !time) {
+      setFirstActive(prev => !prev);
+      onPress && onPress();
+    }
+  };
+
+  return (
+    <Pressable onPress={action}>
+      {toggled || !firstActive ? Second : First}
+    </Pressable>
+  );
+}
--- app/components/TranslatioonCardActions.tsx
+++ app/components/TranslatioonCardActions.tsx
@@ -1,17 +1,23 @@
-import { StarIcon, CopyIcon } from 'lucide-react-native';
+import { StarIcon, CopyIcon, CheckIcon, StarOffIcon } from 'lucide-react-native';
 import { View, Pressable } from 'react-native';
 import * as Clipboard from 'expo-clipboard';
 import { Translation } from '~/service/storage.service';
+import ToggleIcon from './ToggleIcon';
 
 export default function TranslationCardActions({ translation }: { translation: Translation }) {
   return (
     <View className="flex flex-row gap-4 self-end">
-      <Pressable onPress={() => Clipboard.setStringAsync(translation.output)}>
-        <StarIcon size={24} className="text-muted-foreground"/>
-      </Pressable>
-      <Pressable onPress={() => Clipboard.setStringAsync(translation.output)}>
-        <CopyIcon size={24} className="text-muted-foreground" />
-      </Pressable>
+      <ToggleIcon
+        toggled={true}
+        First={<StarIcon size={24} className="text-muted-foreground"/>}
+        Second={<StarOffIcon size={24} className="text-yellow-400"/>}
+      />
+      <ToggleIcon 
+        onPress={() => Clipboard.setStringAsync(translation.output)}
+        time={5000}
+        First={<CopyIcon size={24} className="text-muted-foreground" />} 
+        Second={<CheckIcon size={24} className="text-muted-foreground" />}
+      />
     </View>
   );
 }
